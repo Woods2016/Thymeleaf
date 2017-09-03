271d4cd3b3b3ecce40237dbb9319faea64067c08
Woods2016 <ljz00001@163.com>
2017-09-03 18:10:38 +0800

    v1.0

--- src/main/java/com/zhou/TMController.java
+++ src/main/java/com/zhou/TMController.java
@@ -7,6 +7,7 @@ package com.zhou;
  */
 public class TMController {
     public void getTest(){
-        System.out.println("this is base!");
+        System.out.println("this is base!!");
+        System.out.println("v1.0");
     }
 }
